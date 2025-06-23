1fc285c046ef1e8dfd1f6e8b8097e6884cf6aa27
Michael Rizzo <m_rizzo@live.com>
2025-06-23 11:45:27 -0400

    need to fix checking algorithm. Will work on this later.

--- challenge_chatgpt.py
+++ challenge_chatgpt.py
@@ -1,7 +1,15 @@
 my_nums=[3, -4, 2, -1, 6, -5]
-
+success_tuples_list=[]
+#assuming we have whole positive and negative number the sum we are looking for is 1
+current_sum=5
 def closest_to_zero(my_list):
     for index_1 in range(0,len(my_list)-1):
         for index_2 in range(index_1+1,len(my_list)):
-            print(f"Index 1 is {index_1} and index 2 is {index_2}")
-closest_to_zero(my_nums)
+            my_sum=my_list[index_1]+my_list[index_2]
+            if my_sum <0 or my_sum> current_sum:
+                continue
+            else:
+                success_tuples_list.append((my_list[index_1],my_list[index_2]))
+    return success_tuples_list
+out=closest_to_zero(my_nums)
+print(out)
