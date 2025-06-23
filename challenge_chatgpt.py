65b92fe166324d9b596ec2463928d94f2f86cc67
Michael Rizzo <m_rizzo@live.com>
2025-06-23 11:24:42 -0400

    indexing bug has been fixed.

--- challenge_chatgpt.py
+++ challenge_chatgpt.py
@@ -1,7 +1,7 @@
 my_nums=[3, -4, 2, -1, 6, -5]
 
 def closest_to_zero(my_list):
-    for index_1 in range(0,len(my_list)):
-        for index_2 in range(index_1+1,len(my_list[index_1:])):
+    for index_1 in range(0,len(my_list)-1):
+        for index_2 in range(index_1+1,len(my_list)):
             print(f"Index 1 is {index_1} and index 2 is {index_2}")
 closest_to_zero(my_nums)
