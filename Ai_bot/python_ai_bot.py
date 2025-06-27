450f2ec2338856ad76f7bc29447b0388705accc9
Michael Rizzo <m_rizzo@live.com>
2025-06-27 10:43:43 -0400

    Got 429 issue fixed by adding payment method to open ai account.

--- Ai_bot/python_ai_bot.py
+++ Ai_bot/python_ai_bot.py
@@ -3,18 +3,21 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-my_openai.apikey=os.getenv("openai_key")
+client=my_openai.OpenAI(
+    api_key=os.getenv("openai_key")
+)
 def ask_ai(question,content):
-    response=my_openai.ChatCompletion.create(
-        model="gpt-4",
+    response=client.chat.completions.create(
+        model="gpt-4o",
         messages=[
             {"role": "system","content":content},
             {"role": "user", "content": question}
         ]
     )
-    return response['choices'][0]['message']['content']
+    return response.choices[0].message.content
 
-my_prompt=input("Please give me your question")
+my_prompt=input("Please give me your question\n")
 my_response=ask_ai(my_prompt,"you are a assistant")
+print(my_response)
 
 
